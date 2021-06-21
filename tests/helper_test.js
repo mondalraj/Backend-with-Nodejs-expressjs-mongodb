68ab1c15910baf19922e3f62a6d1d151efeb1cad
Rajib Mondal <mondal021002rajib@gmail.com>
2021-06-21 23:44:35 +0530

    Hooks in Mocha

--- tests/helper_test.js
+++ tests/helper_test.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/mongotube', { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-// Mongodb Connection check [.once(), .on() is special type of promise used with databases]
-mongoose.connection
-    .once('open', () => console.log('Connected'))
-    .on('error', (error) => {
-        console.log("Your Error", error);
-    })
+before((done) => {
+    mongoose.connect('mongodb://localhost/mongotube', { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connection
+        .once('open', () => {
+            // console.log('Connected');
+            done();
+        })
+        .on('error', (error) => {
+            console.log("Your Error", error);
+        })
+})
